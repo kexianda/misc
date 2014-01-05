d3aac79536e677facd3fd7909c98c1e07919cd57
kexianda <kexianda@hotmail.com>
2014-01-05 16:56:35 +0800

    c++11 features Chapter 2

--- 2_1_1.cpp
+++ 2_1_1.cpp
@@ -1,10 +1,28 @@
+/*
+ * Author: Ke, Xianda
+ * Date: 2014-01-05
+ * */
 #include<iostream>
+#include<stdio.h>
+
+#define LOG(...){\
+	fprintf(stderr, "%s: Line %d\t", __FILE__, __LINE__); \
+	fprintf(stderr, __VA_ARGS__);\
+	fprintf(stderr,"\n");\
+}
+
 using namespace std;
 const char*  funName()
 {
+	int x = 3;
+	LOG("x = %d", x);
+
 	return __func__;
 }
 int main()
 {
+	#if __cplusplus < 201103L
+		#error "should use c++11 implementation. please use -std=c++11 option"
+   	#endif
 	cout<<funName()<<endl;
 }
