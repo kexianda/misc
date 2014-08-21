87fbaca5bbe6a1454f28d95466e29498fd9f648d
kexianda <kexianda@hotmail.com>
2014-08-21 22:19:31 +0800

    xtl_construct/destory

--- xtl/test.cpp
+++ xtl/test.cpp
@@ -3,20 +3,44 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include "xmemory.h"
+
+using namespace std;
 
 struct Foo{
 	Foo () { std::cout<<"Foo()" << std::endl; }
 	int val;
-	
+	~Foo () {std::cout<<"~Foo()"<<endl;}	
 };
-void testStl () {
 
-	std::vector<Foo> vf;
-	Foo f1;
-	vf.push_back(f1);
+namespace xtl_memeory_test {
+	
+	void testStl () {
+	
+		std::vector<Foo> vf;
+		Foo f1;
+		vf.push_back(f1);
+	}
+	
+	void testMem (){
+		vector <Foo> vi;
+		for (int i=0; i<3; ++i){
+			vi.push_back (Foo());
+		}
+		auto itr = std::begin (vi);	
+		xtl::destory (itr, itr+2);
+
+		Foo arr[2];
+		Foo * p = arr;
+		xtl::construct (p);
+		xtl::construct (p+1);
+		xtl::destory (p, p+2);//[, )
+	}
 }
 
 int main () {
-	testStl ();
+	
+	xtl_memeory_test::testMem ();
+
 	return 0;
 }
--- xtl/xmemory.h
+++ xtl/xmemory.h
@@ -1,23 +1,76 @@
+/**
+ *
+ *@Author:  Ke, Xianda 
+ *@Date: 2014-08-18
+ *
+ *@Description:  Just for fun.
+ *
+ **/
+
+/**
+ *   Copyright (c) 1994
+ *   Hewlett-Packard Company
+ *  
+ *   Permission to use, copy, modify, distribute and sell this software
+ *   and its documentation for any purpose is hereby granted without fee,
+ *   provided that the above copyright notice appear in all copies and
+ *   that both that copyright notice and this permission notice appear
+ *   in supporting documentation.  Hewlett-Packard Company makes no
+ *   representations about the suitability of this software for any
+ *   purpose.  It is provided "as is" without express or implied warranty.
+ *  
+ **/
+#ifndef _XTL_MEMORY_H
+#define _XTL_MEMORY_H
 
 #include <new>
+namespace xtl {
+	struct _true_type {};
+	struct _false_type {};
 
-struct _true_type {}
-struct _false_type {};
-
-//template<typename T>
-
-
-template<typename T>
-inline void _construct(T*){
-	new (p) T();
+	template <typename T>
+	struct _type_trait {
+		typedef  _false_type has_trivial_destructor;
+	};
+	
+	template <typename T>
+	inline void construct (T* p){
+		new (p) T();
+	}
+	
+	template <typename T>
+	inline void copy_construct (T* p, const T& value) {
+		new (p) T (value);
+	} 
+	
+	template <typename T>
+	inline void _destory (T* p) {
+		delete p;
+	}
+	
+	template <typename FwdItr>
+	inline void _destory_aux (FwdItr first, FwdItr last, _false_type) {
+		//do nothing
+	}
+	
+	template <typename FwdItr>
+	inline void _destory_aux (FwdItr first, FwdItr last,  _true_type) {
+		for (; first < last; ++first) {
+			_destory (& *first);
+		}
+	} 
+	
+	template <typename FwdItr, typename T>
+	inline void _destory_aux (FwdItr first, FwdItr last, T*) {
+		typedef typename _type_trait<T>::has_trivial_destructor trivial_destructor;
+		_destory_aux (first, last, trivial_destructor());
+	}
+	
+	template <typename FwdItr>
+	inline void destory (FwdItr first, FwdItr last) {
+		_destory_aux (first, last, &*first);
+	}
+	
 }
+#endif // _XTL_MEMORY_H
 
-template<typename T>
-inline void _copy_construct (T* p, const T& value) {
-	new (p) T (value);
-} 
-
-template<typename T>
-inline void _destory (T* p) {
-	//_destory_aux (p, 
-}
