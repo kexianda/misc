b20529643de4ea2f97d3ea74c6e646511c1b5acb
kexianda <kexianda@hotmail.com>
2016-02-22 22:41:53 +0800

    pig - update script for building UDFs

--- pig/buildudf.sh
+++ pig/buildudf.sh
@@ -1,3 +1,7 @@
-javac -cp ~/git/pig/build/ivy/lib/Pig/hadoop-common-2.5.0.jar:./pig-0.15.0-SNAPSHOT.jar ./myudfs/DummyCollectableLoader.java
+cp=`dirname ~/git/pig/build/ivy/lib/Pig/hadoop-common-2.5.0.jar`/hadoop-common-2.5.0.jar
+cp=${cp}:`dirname ~/git/pig/build/pig-0.15.0-SNAPSHOT.jar`/pig-0.15.0-SNAPSHOT.jar
+echo ${cp}
+
+javac -cp ${cp} ./myudfs/DummyCollectableLoader.java
 
 jar -cf myudfs.jar myudfs
