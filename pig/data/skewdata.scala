d5fdee7b1467a30a12e61ddcdc33d3a0c3428577
Xianda Ke <xianda.ke@intel.com>
2016-09-14 09:42:35 +0800

    [Pig on Spark] Skewed Join: ut script

--- pig/data/skewdata.scala
+++ pig/data/skewdata.scala
@@ -8,6 +8,32 @@ for (i <- 0 to 5) {
 }
 w.close()
 
+
+// -------TestSkewedJoin UT data --------------
+val u1 = new PrintWriter(new java.io.File("SkewedJoinUT1.txt" ))
+var k = 0
+for (i <- 0 to 120) {
+    u1.write("100\tapple1\taaa" + k + "\n")
+	k = k + 1
+    u1.write("200\torange1\tbbb" + k + "\n")
+	k = k + 1
+    u1.write("300\tstrawberry\tccc" + k + "\n")
+    k = k + 1
+}
+u1.close()
+
+val u2 = new PrintWriter(new java.io.File("SkewedJoinUT2.txt" ))
+u2.write("100\tapple1\n")
+u2.write("100\tapple2\n")
+u2.write("100\tapple2\n")
+u2.write("200\torange1\n")
+u2.write("200\torange2\n")
+u2.write("300\tstrawberry\n")
+u2.write("400\tpear\n")
+u2.close()
+// -----------------------------------------------
+
+
 val w2 = new PrintWriter(new java.io.File("SkewedJoinInput2.txt" ))
 w2.write("100\tapple00001\ta\n")
 w2.write("100\tapple00002\ta\n")
@@ -53,17 +79,18 @@ oneValue.close()
 //---- TestSkewedJoin.testSkewedJoinEmptyInput() ----
 val wEmptyL = new PrintWriter(new java.io.File("SkewedJoinLeft.dat"))
 val wEmptyR = new PrintWriter(new java.io.File("SkewedJoinRight.dat"))
-wEmptyL.println("1")
-wEmptyL.println("2")
-wEmptyL.println("3")
-wEmptyL.println("5")
+wEmptyL.println("1\tleft_one")
+wEmptyL.println("2\tleft_two")
+wEmptyL.println("3\tleft_three")
+wEmptyL.println("5\tleft_five")
 wEmptyL.close()
-wEmptyR.println("1\tone")
-wEmptyR.println("2\ttwo")
-wEmptyR.println("3\tthree")
+wEmptyR.println("1\tright_one")
+wEmptyR.println("2\tright_two")
+wEmptyR.println("3\tright_three")
+wEmptyR.println("6\tright_six")
 wEmptyR.close()
 val empty = new PrintWriter(new java.io.File("testSkewedJoinEmptyInput.pig"))
-empty.println("a = load 'SkewedJoinLeft.dat' as (nums:chararray);")
+empty.println("a = load 'SkewedJoinLeft.dat' as (nums:chararray,text:chararray);")
 empty.println("b = load 'SkewedJoinRight.dat' as (number:chararray,text:chararray);")
 empty.println("c = filter a by nums == '7';")
 empty.println("d = join c by nums LEFT OUTER, b by number USING 'skewed';")
--- pig/skewedjoin.pig
+++ pig/skewedjoin.pig
@@ -1,7 +1,17 @@
- A = load 'SkewedJoinInput_small.txt' as (id:int, name, n);
- B = load 'SkewedJoinInput2.txt' as (id:int, name, tinfo);
- C = join A by (id, name), B by (id, name)  using 'skewed' parallel 7;
- dump C;
+ 
+ -- testSkewedJoinWithGroup
+ A = load 'SkewedJoinUT1.txt' as (id, name, n);
+ B = load 'SkewedJoinUT2.txt' as (id, name);
+ C = group A by id;
+ D = group B by id;
+ E = join C by group, D by group using 'skewed' parallel 5;
+ dump E;
+
+--A = load 'SkewedJoinInput_small.txt' as (id:int, name, n);
+-- B = load 'SkewedJoinInput2.txt' as (id:int, name, tinfo);
+-- C = join A by (id, name), B by (id, name) using 'skewed' parallel 7;
+--C = join A by id, B by id using 'skewed' parallel 7;
+-- dump C;
 
 -- plain join
 -- A = load 'joinleft.txt' as (id:int, name);
