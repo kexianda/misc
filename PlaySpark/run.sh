91139d92109e5ff8da79b2319f56401eaa4bfc52
Xianda Ke <kexianda@gmail.com>
2017-06-26 10:49:53 +0800

    gen test data using spark sql

--- PlaySpark/run.sh
+++ PlaySpark/run.sh
@@ -1,2 +1,5 @@
-$SPARK_HOME/bin/spark-submit ./target/scala-2.11/playspark_2.11-1.0.jar
+
+hdfs dfs -rm -r testdata.parquet
+
+$SPARK_HOME/bin/spark-submit --executor-memory 8g --driver-memory 8g ./target/scala-2.11/playspark_2.11-1.0.jar
 
--- PlaySpark/src/main/scala/PlaySql.scala
+++ PlaySpark/src/main/scala/PlaySql.scala
@@ -13,38 +13,69 @@ object PlaySql {
       val spark = SparkSession
         .builder().
         appName("investigating index").
-        config("spark.sql.parquet.compression.codec", "uncompressed")
+        config("spark.sql.parquet.compression.codec", "snappy")
         .getOrCreate()
 
+      // uncompressed
       //sc.hadoopConfiguration.setInt( "dfs.blocksize", blockSize)
       //sc.hadoopConfiguration.setInt( "parquet.block.size", blockSize *8)
 
-      val recordsSize = 1024
+      val recordsNum = 1024 * 1024 * 5 
       val cal = Calendar.getInstance
 
-      val salesRDD = spark.sparkContext.makeRDD( (1 to recordsSize).map(
-        i => {
-          cal.set(2017, 6, i%10 + 1)
-          val r = new Random()
-          val name = "name" + i % 1000
-          val item = "item" + i % 50
-          val quantity = r.nextInt(100) + 1
-          val price = 1000 + (r.nextDouble() * 100)
-          val date = new java.sql.Date(cal.getTimeInMillis)
-          Sales(name, item, quantity, price, price * quantity, date, date)
-        }), 1)
+      val salesRDD = spark.sparkContext.makeRDD((1 to recordsNum).map { i =>
+        cal.set(2017, 6, 1)
+        val r = new Random()
+        val name = "name" + i % 1000
+        val item = "item" + i % 50
+        val quantity = r.nextInt(100) + 1
+        val price = 1000 + (r.nextDouble() * 100)
+        val date = new java.sql.Date(cal.getTimeInMillis)
+        Sales(name, item, quantity, price, price * quantity, date, date)
+      }, 1)
+
+//      val slices = 10
+//      val numPerPartion = 10
+//      val rdd = spark.sparkContext.parallelize((1 to slices), slices)
+//      val salesRDD = rdd.flatMap { p =>
+//        (1 to numPerPartion).map { i =>
+//          cal.set(2017, 5, p % 10 + 1)
+//          val r = new Random()
+//          val name = "name" + i % 1000
+//          val item = "item" + i % 50
+//          val quantity = r.nextInt(100) + 1
+//          val price = 1000 + (r.nextDouble() * 100)
+//          val date = new java.sql.Date(cal.getTimeInMillis)
+//          Sales(name, item, quantity, price, price * quantity, date, date)
+//        }
+//      }
+//
+//      val salesRDD = spark.sparkContext.makeRDD( (1 to recordsNum).map(
+//        i => {
+//          cal.set(2017, 6, i%10 + 1)
+//          val r = new Random()
+//          val name = "name" + i % 1000
+//          val item = "item" + i % 50
+//          val quantity = r.nextInt(100) + 1
+//          val price = 1000 + (r.nextDouble() * 100)
+//          val date = new java.sql.Date(cal.getTimeInMillis)
+//          Sales(name, item, quantity, price, price * quantity, date, date)
+//        }), 1)
 
       import spark.implicits._
       val salesDS = spark.sqlContext.createDataset(salesRDD)
-      //salesDS.show()
-
-      //salesDS.write.partitionBy("doc_date").parquet("testdata.parquet")
       salesDS.write.partitionBy("doc_date").parquet("testdata.parquet")
 
+      // query some
       val allSales = spark.read.parquet("testdata.parquet")
       allSales.createOrReplaceTempView("sales")
-      val q1DF = spark.sql("SELECT * FROM sales WHERE doc_date = '2017-07-02' ")
+      val q1DF = spark.sql("SELECT * FROM sales WHERE doc_date = '2017-07-01' ")
       q1DF.show()
+      //q1DF.write.
+
+//      val q2 = spark.sql("INSERT INTO sales SELECT * FROM sales")
+//      q2.write.mode("append").partitionBy("doc_date").parquet("testdata.parquet")
+      spark.stop()
     }
     catch {
       case e: Exception => println(e.getMessage)
