349b09bdd7a6267d069158daee6e55fbdc78bf02
Xianda Ke <kexianda@gmail.com>
2017-08-22 16:55:10 +0800

    ut & memcpy, todo: cache write conflict?

--- memfun/driver.cpp
+++ memfun/driver.cpp
@@ -7,8 +7,17 @@
 using namespace std;
 
 //#define memcpy  _i_memcpy_256_unaligned
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern char* _i_memcpy_256_unaligned(char* di, char* si, int dx);
+extern char* _i_memcpy_256_unaligned(char* di, const char* si, const int dx);
+
+#ifdef __cplusplus
+}
+#endif
+
+
 void testMem();
 /*
  *
@@ -19,6 +28,24 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+void verify(const char* src, const char* des, 
+        const int offset1, const int offset2, const int len,
+        const int origSize) {
+    for (int i = 0; i< origSize; i++) {
+        if( i < offset2 || i >= (offset2 +len)) {
+            if ((*(des + i) != '\0'))
+                printf("Error at %d, should be \\0, but %c", i, *(des + i));
+        }        
+        else { // compare
+            if (*(src + offset1 - offset2 + i) != *(des + i)) {
+                printf("Error at %d, should be %c, but %c", 
+                    i, *(src + offset1 - offset2 + i), *(des + i));
+            }
+        }
+    }
+    printf("Successful!");
+}
+
 void testMem() {
 
 //    const auto t1 = std::chrono::high_resolution_clock::now();
@@ -26,18 +53,46 @@ void testMem() {
 //    const std::chrono::duration<double> td = t2 - t1;
 //    printf("reference result = %lu, time = %10.6f s\n", result, td.count());
 
-    int size = 256 + 128 + 64 + 32 + 16 + 9;
-    char* s1 = (char*)malloc(sizeof(char) * size);
-    char* s0 = (char*)malloc(sizeof (char) * (size + 5));
-    char* s2 = s0 + 5; //not 16 aligned!
-    for(int i =0; i < size; i++)
-        s1[i] = 'a' + i%26;
-
-    _i_memcpy_256_unaligned(s2, s1, size);
-    for (int i = 0; i < size; i++)
-        s2[i] = 0;
-
-    _i_memcpy_256_unaligned(s2, s1, size);
+    //int size = 256 + 128 + 64 + 32 + 16 + 9; // 505
+    int origSize = 1024 * 8; //two pages
+    char* s1 = (char*) malloc(sizeof (char) * origSize);
+    char* s2 = (char*) malloc(sizeof (char) * origSize);
+    for (int i = 0; i < origSize; i++) 
+        s1[i] = ('a' + i%26);
+    memset(s2, '\0', origSize);
+    
+    // not aligned
+    {
+        int offset1 = 26;
+        int offset2 = 5;
+        const char* src = s1 + offset1; 
+        char* des = s2 + offset2; 
+        int len = 256 + 128 + 64 + 32 + 16 + 9; // 505
+        _i_memcpy_256_unaligned(des, src, len);
+        verify(s1, s2, offset1, offset2, len, origSize);
+    }
+        
+    {
+        int offset1 = 0;
+        int offset2 = 0;
+        const char* src = s1 + offset1;
+        char* des = s2 + offset2; 
+        
+        // break __memcpy_avx_unaligned
+        memset(s2, '\0', origSize);
+        
+        memcpy(des, src, 7);
+        memcpy(des, src, 15);
+        memcpy(des, src, 31);
+        memcpy(des, src, 31);
+        memcpy(des, src, 63);
+        memcpy(des, src, 127);
+        memcpy(des, src, 254);
+        memcpy(des, src, 511);
+        memcpy(des, src, 2047);
+        memcpy(des, src, 8193);
+    }
+        
     free(s2);
     free(s1);
 }
